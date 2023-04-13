ef0b4a3200e4815fb523345612627d62e32f7cd3
Randi Wilson <randi274@gmail.com>
2023-04-13 16:43:55 -0400

    ci: add origin for changelog

--- scripts/change-log-generator-utils.js
+++ scripts/change-log-generator-utils.js
@@ -64,7 +64,7 @@ function getPreviousReleaseBranch() {
  function getCommits(releaseBranch, previousBranch) {
   const commits = shell
     .exec(
-      `git log --cherry-pick --oneline ${releaseBranch}...${previousBranch}`,
+      `git log --cherry-pick --oneline origin/${releaseBranch}...origin/${previousBranch}`,
       {
         silent: false
       }
